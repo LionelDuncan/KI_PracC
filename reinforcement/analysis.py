66d15308d253c7f5e762a622f87538a6274303c4
Unknown <lionel.vo1997@gmail.com>
2017-12-21 14:20:43 +0100

    q1 + q2 + q3

--- reinforcement/analysis.py
+++ reinforcement/analysis.py
@@ -27,7 +27,7 @@ def question2():
 def question3a():
     answerDiscount = 0.3
     answerNoise = 0
-    answerLivingReward = 0.01
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
@@ -41,7 +41,7 @@ def question3b():
 def question3c():
     answerDiscount = 0.9
     answerNoise = 0
-    answerLivingReward = 0.1
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
@@ -54,7 +54,7 @@ def question3d():
 
 def question3e():
     answerDiscount = 1
-    answerNoise = 0.2
+    answerNoise = 0
     answerLivingReward = 1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
